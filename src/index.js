f250ab1adaaadf7cf86a46e643d89c62f2f8729c
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-22 17:23:14 +0200

    Syn working directory

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import "./css/normalize.css";
 import "./css/base.css";
 import JsonToClasses from "./modules/JsonToClasses";
-import { TodoItem, TodoProject } from "./modules/todoData";
-import JSON_PROJ_DATA from "./data/PROJECTS.json";
-import { InternalState, LocalStorageAPI } from "./modules/InternalState";
+import InternalState from "./modules/InternalState";
+import INITIAL_STATE_DATA from "./data/INITIAL_STATE_DATA.json";
 
-const state = new InternalState(JsonToClasses.parseAllData(JSON_PROJ_DATA));
-
-let stateJSON = state.state;
-console.log(stateJSON);
-
-// // state.deleteProject(id);
-// state.addProject(new TodoProject({ title: "hello world" }));
-// console.log(state.getState());
-// state.syncStateToLocalStorage();
+const todoState = new InternalState(JsonToClasses.parseAllData(INITIAL_STATE_DATA));
+console.log(todoState.state);
