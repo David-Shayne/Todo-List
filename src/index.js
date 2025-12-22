f6f5f34502eadbc2fe98ef6445d02041ac49dde4
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-22 10:20:56 +0200

    Modify index for testing internal state instance

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import "./css/normalize.css";
 import "./css/base.css";
-import dataParser from "./modules/dataParser";
+import JsonToClasses from "./modules/JsonToClasses";
 import { TodoItem, TodoProject } from "./modules/todoData";
 import JSON_PROJ_DATA from "./data/PROJECTS.json";
+import InternalState from "./modules/InternalState";
 
-const items = dataParser.parseProjectArray(JSON_PROJ_DATA);
-console.log(items);
+const state = new InternalState(JsonToClasses.parseAllData(JSON_PROJ_DATA));
+const id = "a";
+const project1 = state.getProject(id);
+console.log(project1);
