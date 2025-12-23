bda969456d87415eb17ffb9bcb4cde31882e1f89
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-23 10:59:17 +0200

    Hide index.js to avoid testing commits

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import "./css/normalize.css";
 import "./css/base.css";
-import JsonToClasses from "./modules/JsonClassesConverter";
+import JsonClassesConverter from "./modules/JsonClassesConverter";
 import InternalState from "./modules/InternalState";
 import INITIAL_STATE_DATA from "./data/INITIAL_STATE_DATA.json";
+import LocalStorageAPI from "./modules/LocalStorageAPI";
 
-const todoState = new InternalState(JsonToClasses.getJsonToStateObject(INITIAL_STATE_DATA));
+const todoState = new InternalState();
 console.log(todoState.getState());
+todoState.setState(JsonClassesConverter.getJsonToStateObject(INITIAL_STATE_DATA));
+console.log(todoState.getState());
+const Json = JsonClassesConverter.getStateObjectToJson(todoState.getState());
+
+LocalStorageAPI.storeState(Json);
