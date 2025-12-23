d828d8417a5fbc91ff1301719b8c3008d1a281ca
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-23 12:16:34 +0200

    Fix initial JSON data and setup main functions for setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,34 @@ import JsonClassesConverter from "./modules/JsonClassesConverter";
 import InternalState from "./modules/InternalState";
 import INITIAL_STATE_DATA from "./data/INITIAL_STATE_DATA.json";
 import LocalStorageAPI from "./modules/LocalStorageAPI";
+import { TodoProject } from "./modules/todoClasses";
 
-InternalState.setState(JsonClassesConverter.getJsonToStateObject(INITIAL_STATE_DATA));
-console.log(InternalState.getState());
-const Json = JsonClassesConverter.getStateObjectToJson(InternalState.getState());
+// Populates given Json state with all missing default properties
+function getPopulatedJsonData(INITIAL_STATE_DATA) {
+	return JsonClassesConverter.getStateObjectToJson(
+		JsonClassesConverter.getJsonToStateObject(INITIAL_STATE_DATA)
+	);
+}
 
-LocalStorageAPI.storeState(Json);
+// Registers whether there is a localStorage state saved and either loads that or loads given data in JSON.
+function initLocalStorageState(INITIAL_STATE_DATA) {
+	if (!LocalStorageAPI.getState()) {
+		LocalStorageAPI.storeState(INITIAL_STATE_DATA);
+	}
+}
+
+// Push internal state to local state (sync them)
+function syncLocalState(internalState) {
+	LocalStorageAPI.storeState(JsonClassesConverter.getStateObjectToJson(internalState));
+}
+
+function syncInternalState(localState) {
+	InternalState.setState(JsonClassesConverter.getJsonToStateObject(localState));
+}
+
+// Turn initial JSON object data given to JSON string data for localStorage
+INITIAL_STATE_DATA = JSON.stringify(INITIAL_STATE_DATA);
+INITIAL_STATE_DATA = getPopulatedJsonData(INITIAL_STATE_DATA);
+
+initLocalStorageState(INITIAL_STATE_DATA);
+syncInternalState(LocalStorageAPI.getState());
