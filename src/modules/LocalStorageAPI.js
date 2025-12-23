b576792adc4f546afdd2e902d662a7a9dfe5f7b7
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-23 10:58:25 +0200

    Change name of LocalStorageAPI and populate with methods and properties (static)

--- src/modules/LocalStorageAPI.js
+++ src/modules/LocalStorageAPI.js
@@ -1,2 +1,14 @@
-// Single Responsibility is to transfer data to and from LocalStorage and InternalState instance
-export default class LocalStorageAPI {}
+// Single Responsibility is to transfer data to and from LocalStorage and InternalState instance in the JSON format
+export default class LocalStorageJsonAPI {
+	static #storageName = "todoState";
+	static #state = {};
+
+	static storeState(stateJSON) {
+		localStorage[this.#storageName] = stateJSON;
+		this.#state = stateJSON;
+	}
+
+	static getState() {
+		return this.#state;
+	}
+}
