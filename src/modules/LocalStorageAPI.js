6974cce1724e06e91ebe9d8faf4e99bf85454961
David-Shayne <63462420+David-Shayne@users.noreply.github.com>
2025-12-23 11:29:14 +0200

    Change LocalStorageAPI to a pure functional instance and remove internal state for cleaner code

--- src/modules/LocalStorageAPI.js
+++ src/modules/LocalStorageAPI.js
@@ -1,14 +1,12 @@
 // Single Responsibility is to transfer data to and from LocalStorage and InternalState instance in the JSON format
 export default class LocalStorageJsonAPI {
 	static #storageName = "todoState";
-	static #state = {};
 
 	static storeState(stateJSON) {
 		localStorage[this.#storageName] = stateJSON;
-		this.#state = stateJSON;
 	}
 
 	static getState() {
-		return this.#state;
+		return localStorage[this.#storageName];
 	}
 }
